0d2d23de28fe3010a95b3dab043db8db1a9aa196
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-01-27 14:28:48 -0600

    initial commit

---
+++ benchmark.js
@@ -0,0 +1,19 @@
+const shuffle = require('./exec.js');
+const mergeSort = require('./merge-sort.js');
+
+for( let i = 1; i < 8; i++ ){
+  let length = 10 ** i
+  let array = new Array( length );
+
+  for( let i = 0; i < array.length; i++ ){
+    array[i] = i;
+  }
+
+  shuffle(array);
+
+  let str = `sorting ${length}`;
+  console.time(str);
+  mergeSort(array);
+  console.timeEnd(str);
+
+}
