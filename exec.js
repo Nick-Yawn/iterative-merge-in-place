c1aa8382fad00ff6f927744fff6bacc81dde69ae
Nicholas Yuan <NicholasNYuan@gmail.com>
2025-01-21 15:04:53 -0600

    cleanup

--- exec.js
+++ exec.js
@@ -2,8 +2,6 @@ const mergeSort = require('./merge-sort');
 const shuffle = require('./shuffle');
 
 const ARRAY_LENGTH = 15;
-const MIN_CARDS = 0; // per shuffle step
-const MAX_CARDS = 3; // per shuffle step
 
 const isSorted = array => {
   for( let i = 0; i < array.length - 1; i++ ){
@@ -11,52 +9,6 @@ const isSorted = array => {
   }
   return true;
 }
-
-/* OLD SHUFFLE
-const shuffle = array => { // it's not random, but it's good enough
-  let halfIndex = Math.floor( array.length / 2 );
-  for( let i = 0; i < 4; i++ ){
-    //console.log(`shuffling ${i}`);
-    let firstHalf = array.slice( 0, halfIndex );
-    let secondHalf = array.slice( halfIndex );
-    
-    // first and second half will be treated as queue, 
-    // inserting 1-2 "cards" from each, alternating
-    // however, shift is slow — O(n) — so we fake the queue.
-    let firstIndex = 0;
-    let secondIndex = 0;
-    let indexActual = 0;
-  
-    while( firstIndex < firstHalf.length || secondIndex < secondHalf.length){
-      let numCards;
-      let distanceToEnd;
-
-      if( firstIndex < firstHalf.length ){
-        numCards = Math.floor(Math.random() * (MAX_CARDS - MIN_CARDS + 1) + MIN_CARDS); // could put this in a helper function
-        distanceToEnd = firstHalf.length - firstIndex;
-        for( let i = 0; i < (numCards < distanceToEnd ? numCards : distanceToEnd ) ; i++ ){
-          array[indexActual++] = firstHalf[firstIndex++]; // ++ increments AFTER returning the value. to increment before, use ++x
-        }
-      }
-
-      if( secondIndex < secondHalf.length ){ // could actually put both of these halves into a helper function.... but may be more trouble than it's worth
-        numCards = Math.floor(Math.random() * (MAX_CARDS - MIN_CARDS + 1) + MIN_CARDS);
-        distanceToEnd = secondHalf.length - secondIndex;
-        for( let i = 0; i < (numCards < distanceToEnd ? numCards : distanceToEnd ) ; i++ ){
-          array[indexActual++] = secondHalf[secondIndex++];
-        }
-      }
-    } 
-
-    // swap first card and random other card in the middle
-    let arrayLength = array.length
-    let swapIndex = Math.floor(Math.random() * (arrayLength * .75 - arrayLength * .25 + 1) + arrayLength * .25);
-    let temp = array[0];
-    array[0] = array[swapIndex];
-    array[swapIndex] = temp;      // yes, I can use destructuring, but being able to swap WITHOUT destructuring is important.
-  }
-}
-*/
 
 // make array
 let arr = new Array(ARRAY_LENGTH);
--- merge-sort.js
+++ merge-sort.js
@@ -1,24 +1,10 @@
 const DEBUG = false;
 
 const mergeSort = (arr, verbose = false) => {
-  // are you ready for an adventure?
 
   let queue = new Array(arr.length * 2);    
   let queueIndex = 0; // using shift is expensive, so we're gonna fake the queue (again).
   
-  let firstUnqueuedIndex;       // declaring loop variables up here saves program from
-  let secondUnqueuedIndex;      //    doing extra memory management. not sure if it makes a difference
-                                // JS is not the language to care about such low-level optimizing
-  let nextFirstUnqueuedIndex;   // ...probably
-  let nextSecondUnqueueIndex;   
-  let sortingIndex;             // UPDATE: I did some testing and this seems to actually make things slower. Will fix it later. 
-  let nextSortingIndex;         // Don't let this be your takeaway! Declare your variables at the lowest scope required!
-  let insertionIndex;
-  let sub = new Array(arr.length);
-  let nextSub = new Array(arr.length);
-  let subHasEls;
-  let nextSubHasEls;
-
   // the strange thing is, this queue setup crashes at 5592406 iterations,
   // no matter if doing it forward or backward.
   // This is some kind of optimization bug in Chromium.
@@ -34,7 +20,6 @@ const mergeSort = (arr, verbose = false) => {
   } catch (e) {
     console.log( i, j, e);
   }
-  // these subarrays are 'sorted'
 
   // does this setup look familiar? we're traversing and collapsing a tree of index-pairs.
   //  Maybe there is a better way.
@@ -43,14 +28,14 @@ const mergeSort = (arr, verbose = false) => {
     if (DEBUG) console.log( arr.join(' ') );
 
     // these two values represent the start and end indices of our first subarray. always already sorted
-    firstUnqueuedIndex = queue[queueIndex++];
-    secondUnqueuedIndex = queue[queueIndex++]; // x++ increments AFTER the return.
+    let firstUnqueuedIndex = queue[queueIndex++];
+    let secondUnqueuedIndex = queue[queueIndex++]; // x++ increments AFTER the return.
 
     if( firstUnqueuedIndex === 0 && secondUnqueuedIndex === arr.length - 1 ) break; // if the subarray is the whole array, done! 
  
     // ditto 37 but for the next subarray.
-    nextFirstUnqueuedIndex = queue[queueIndex++]; 
-    nextSecondUnqueuedIndex = queue[queueIndex++]; 
+    let nextFirstUnqueuedIndex = queue[queueIndex++]; 
+    let nextSecondUnqueuedIndex = queue[queueIndex++]; 
  
     //sort! 
     if( secondUnqueuedIndex + 1 !== nextFirstUnqueuedIndex ){ //if not adjacent, requeue first subarray
@@ -62,32 +47,24 @@ const mergeSort = (arr, verbose = false) => {
     } else { // sort, push new subarray indices
       
       // now for in-place sorting between subarrays
-      sortingIndex = 0;
-      nextSortingIndex = 0;
+      let sortingIndex = 0;
+      let nextSortingIndex = 0;
 
       // I don't like this but don't know how else to do this part
       // it's possible to override all the first subarrays values
       // so how else can I keep track of them?
       // oh well, it's O(n)  
       // could do this with a for loop in order to use fixed subarrays in memory.
-      // UPDATE: I did the optimizing. Runtime for 10,000,000 went from 3966ms > 2723ms. Wow!
-      // UPDATE UPDATE: I broke something in the process! nevermind! changed back to what it was at first.
-      sub = arr.slice(firstUnqueuedIndex, secondUnqueuedIndex + 1);
-    /*  for( let i = 0; i < secondUnqueuedIndex - firstUnqueuedIndex + 1; i++ ){
-        sub[i] = arr[firstUnqueuedIndex + i];
-      }*/ 
-      nextSub = arr.slice(nextFirstUnqueuedIndex, nextSecondUnqueuedIndex + 1);
-     /* for( let i = 0; i < nextSecondUnqueuedIndex - nextFirstUnqueuedIndex + 1; i++ ){
-        nextSub[i] = arr[nextFirstUnqueuedIndex + 1];
-      }*/
+      let sub = arr.slice(firstUnqueuedIndex, secondUnqueuedIndex + 1);
+      let nextSub = arr.slice(nextFirstUnqueuedIndex, nextSecondUnqueuedIndex + 1);
 
       if (DEBUG) console.log("sub:\t", sub, "\n", "nextSub:", nextSub);     
       if (verbose) printArr(arr, firstUnqueuedIndex, secondUnqueuedIndex, nextFirstUnqueuedIndex, nextSecondUnqueuedIndex);
 
-      insertionIndex = firstUnqueuedIndex;
+      let insertionIndex = firstUnqueuedIndex;
 
-      subHasEls = true;
-      nextSubHasEls = true;
+      let subHasEls = true;
+      let nextSubHasEls = true;
 
       while( subHasEls || nextSubHasEls ){
         if( !subHasEls ){ // only nextSubHasEls
